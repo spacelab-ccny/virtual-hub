b0a4dd16152742b9aa49facde6777a3a9fb887d0
AniaK16 <ark2219@columbia.edu>
2024-07-23 14:57:33 -0400

    everything as needed to run mpc globally outputing if a device should change states

---
+++ device3_command.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+args=("$@")
+python3 arg_comp.py -M3 -I${args[0]} 0 0 72 0 0 0 0 0 0
--- terminal_sim.sh
+++ terminal_sim.sh
@@ -3,7 +3,6 @@
 #gnome-terminal -e cat test.txt&
 #gnome-terminal -e cat test2.txt &
 
-gnome-terminal -- bash -c "python3 generate_command.py; bash command.sh 0; exec bash"
-gnome-terminal -- bash -c "python3 generate_command.py; bash command.sh 1; exec bash"
-gnome-terminal -- bash -c "python3 generate_command.py; bash command.sh 2; exec bash"
-gnome-terminal -- bash -c "python3 generate_command.py; bash command.sh 3; exec bash"
+gnome-terminal -- bash -c "python3 generate_command.py -i device1_input.xml; bash device1_command.sh 0; exec bash"
+gnome-terminal -- bash -c "python3 generate_command.py -i device2_input.xml; bash device2_command.sh 1; exec bash"
+gnome-terminal -- bash -c "python3 generate_command.py -i device3_input.xml; bash device3_command.sh 2; exec bash"
