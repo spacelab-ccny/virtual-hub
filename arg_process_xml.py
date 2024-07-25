67a30ee895e8d2a9b784e6b3084c270026a9e893
AniaK16 <ark2219@columbia.edu>
2024-07-25 15:01:35 -0400

    works for original input file

--- arg_process_xml.py
+++ arg_process_xml.py
@@ -1,17 +1,5 @@
 #!/bin/python3
-#File used to process input xml file and generate a python script that uses mpyc to create a multiparty computation scheme between all involved devices
-#what we need from the xml
-#number of parties
-#initial arguments for each party
-#the rules for each party
-#a way to check the conditions of the rules for each party
-
-#current plan is to have each device's individual xml file eb the same as the initial xml file but with 0s where it doesn't need the values
-#if we instead have each xml only including the information relevant to that device it won't know how many arguments to pass in
-#process:
-#intial xml file used to generate the computation script
-#python script that process initial xml and generates computation python script
-#at this point we have a computation script that can be run with the appropriate number of parties and arguments passed in
+#Current plan: anywhere we write the conditional comparison to the file, we now, instead, just store the string in a list. Then at the end we can just iterate through that list of strings and write them all
 
 from bs4 import BeautifulSoup
  #taken from https://www.geeksforgeeks.org/reading-and-writing-xml-files-in-python/
@@ -29,32 +17,20 @@ soup = BeautifulSoup(data, "xml")
  
 num_devices= len(soup .find_all('device'))
 print(num_devices)
- 
-# Using find() to extract attributes the function
-# of the first instance of the tag
-b_name = soup .find('device', {'id':'1'})
-val = b_name.find('deviceName').text
-print(val)
-
-
-b_name = soup.find('device', {'id':'3'})
-val = b_name.find('depGroup', {'id':'1'})
-gfg = soup.find(lambda tag: tag.name == "deviceName" and 'Fan' in tag.text)
-
-
- 
-print(val)
-#print(gfg.parent)
 
 #writing the python file:
 f  = open('arg_comp.py', 'w')
 f.write("#!/bin/python3")
-f.write("\nfrom mpyc.runtime import mpc\nimport sys\n\nasync def main():\n\tsecint = mpc.SecInt(16)\n\tawait mpc.start()")
+f.write("\nfrom mpyc.runtime import mpc\nimport sys\n\nasync def main():\n\tsecint = mpc.SecInt(16)")
 #f.write("\n\tif mpc.parties != {}:\n\t\tprint(\"Did not reach expected number of parties. Expected \", await mpc.output({}))\n".format(num_devices, num_devices))
 
 count = 0
 name_list = []
 arg_list = []
+complete_var_list=[]
+all_state_changes=[]
+
+all_comp=[]
 
 #looping through the devices and creating a variable in the output script for each argument
 for i in range(num_devices):
@@ -63,23 +39,26 @@ for i in range(num_devices):
     name = b_name.find('deviceName').text
     num_args = len(b_name.find_all('argument'))
     name_list = name_list + [name]
-    for j in range(num_args):
-        j=j+1
-        count = count +1
-        f.write("\n\t{}{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(name,j,count,count))
-        arg_list = arg_list + [name+str(j)]
-print(name_list)
-f.write("\n")
-for i in range(len(arg_list)):
-    f.write("\n\t{}_combined = sum(mpc.input({}, range({})))".format(arg_list[i],arg_list[i], num_devices))
+    
     
 f.write("\n")
 #now we need to retrieve each of the conditions to change the state of each argument of each device:
 #first we retrieve the dependencies of the device
 for dev in range(len(name_list)):
+    var_list = []
+    comparison_list = []
+    state_changes =[]
     f.write(f"\n\t#{name_list[dev]}")
     dev =dev+1
     device = soup.find('device', {'id':str(dev)})
+    num_args = len(device.find_all('argument'))
+    for j in range(num_args):
+        j=j+1
+        count = count +1
+        f.write("\n\t{}{} = None".format(name_list[dev-1],j))
+        var_list = var_list +[f"{name_list[dev-1]}{j}"]
+        #f.write("\n\t{}{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(name,j,count,count))
+        arg_list = arg_list + [name+str(j)]
     #print(device)
     dependencies = device.find_all('depGroup')
     num_dep = len(dependencies)
@@ -94,38 +73,38 @@ for dev in range(len(name_list)):
         f.write(f"\n\t#Dependency {dep}")
 #Get vaule of variable with devicename.text+decvicenamenum
         dep_group = device.find('depGroup', {'id':str(dep)})
-        state = dep_group.find('stateChange')['num']
-        print(f"state {state}")
-        curr_dev = name_list[dev-1]+str(state)+"_combined"
-        print(curr_dev)
-#read in thresholds into secure variables-- can't do this because the value will be dispalyed in the python script
-        #print(dep_group)
         dep_devs=dep_group.find_all('depDevice')
         #print(dep_devs)
 
         for ddev in range(len(dep_devs)):
-            cur_dep= dep_devs[ddev].text+dep_devs[ddev]['num']+'_combined'
+            cur_dep= dep_devs[ddev].text+dep_devs[ddev]['num']
             print(cur_dep)
             thresh =-1
             try:
                 upper = dep_group.find('upperThreshold',{'arg':ddev+1}).text
-                f.write(f"\n\tutruth{dep} = -1")
+                #f.write(f"\n\tutruth{dep} = -1")
                 thresh =2
                 count = count +1
-                f.write("\n\tupper{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(dep, count, count))
-                f.write("\n\tupper{} = sum(mpc.input(upper{}, range({})))".format(dep, dep, num_devices))
-                f.write(f"\n\tutruth{dep} =  {cur_dep} <  upper{dep}")
+                #f.write("\n\tupper{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(dep, count, count))
+                f.write("\n\t{}_upper{}_{} = None".format(name_list[dev-1],dep,ddev))
+                var_list = var_list +[f"{name_list[dev-1]}_upper{dep}_{ddev}"]
+                #f.write("\n\tupper{} = sum(mpc.input(upper{}, range({})))".format(dep, dep, num_devices))
+                comparison_list = comparison_list + [f"\n\t\tutruth{dep} =  {cur_dep} <{name_list[dev-1]}_upper{dep}_{ddev}"]
+                #f.write(f"\n\tutruth{dep} =  {cur_dep} <  upper{dep}")
                 or_list = or_list +["utruth"+str(dep)]
 
             except:
                 upper = 'none'
             try:
                 lower = dep_group.find('lowerThreshold',{'arg':ddev+1}).text
-                f.write(f"\n\tltruth{dep} = -1")
+                #f.write(f"\n\tltruth{dep} = -1")
                 count = count +1
-                f.write("\n\tlower{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(dep, count, count))
-                f.write("\n\tlower{} = sum(mpc.input(lower{}, range({})))".format(dep, dep, num_devices))
-                f.write(f"\n\tltruth{dep} = {cur_dep} > lower{dep}")
+                #f.write("\n\tlower{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(dep, count, count))
+                f.write("\n\t{}_lower{}_{} = None".format(name_list[dev-1],dep, ddev))
+                var_list = var_list +[f"{name_list[dev-1]}_lower{dep}_{ddev}"]
+                #f.write("\n\tlower{} = sum(mpc.input(lower{}, range({})))".format(dep, dep, num_devices))
+                #f.write(f"\n\tltruth{dep} = {cur_dep} > lower{dep}")
+                comparison_list = comparison_list + [f"\n\t\tltruth{dep} =  {cur_dep} > {name_list[dev-1]}_lower{dep}_{ddev}"]
                 or_list = or_list +["ltruth"+str(dep)]
                 if thresh ==-1:
                     thresh = 1
@@ -133,11 +112,12 @@ for dev in range(len(name_list)):
                 thresh =0
                 lower = 'none'
             if(thresh==2):
-                f.write("\n")
+                #f.write("\n")
                 or_list = or_list[:-2]
-                f.write(f"\n\tin_range{dep} = ltruth{dep} * utruth{dep}")
+                #f.write(f"\n\tin_range{dep} = ltruth{dep} * utruth{dep}")
+                comparison_list = comparison_list + [f"\n\t\tin_range{dep} = ltruth{dep} == utruth{dep}"]
                 or_list = or_list +["in_range"+str(dep)]
-                f.write("\n")
+                #f.write("\n")
                 
             print(f"upper {upper} lower {lower}")
             f.write("\n")
@@ -150,14 +130,21 @@ for dev in range(len(name_list)):
         or_list = or_list[:-len(dep_devs)]
         or_list = or_list + [dep_str]
 
-        f.write(f"\n\ttruth = {dep_str}")
-        for num in range(len(name_list)):
-            if(num+1==dev):
-                f.write(f"\n\tif mpc.pid == {dev-1}:")
-                f.write(f"\n\t\tprint('Condition {dep} met?:',await mpc.output(truth))")
-            else:
-                f.write(f"\n\tif mpc.pid == {num}:")
-                f.write(f"\n\t\tprint('Wait',await mpc.output(truth))")
+        states = dep_group.find('stateChange')
+        #print(f"state {states}")
+        state_count = 0
+       # f.write(f"\n\ttruth = {dep_str}")
+        comparison_list = comparison_list + [f"\n\t\ttruth{dev}_{dep-1} = {dep_str}"]
+        for state in states:
+            count = count+1
+            f.write("\n\t{}_state{} = None".format(name_list[dev-1], dep))
+            var_list = var_list +[f"{name_list[dev-1]}_state{dep}"]
+            state_changes = state_changes + [f"{name_list[dev-1]}_state{dep}"]
+            #f.write("\n\t{}_state{}_{} = secint(int(sys.argv[{}])) if sys.argv[{}:] else secint(0)".format(name_list[dev-1], int(state)+1, dep, count, count))
+       #     f.write("\n\t{}_state{}_{} = sum(mpc.input({}_state{}_{}, range({})))".format(name_list[dev-1], int(state)+1, dep, name_list[dev-1], int(state)+1, dep, num_devices))
+       #     f.write("\n\t{}_state{}_{}_change = truth * {}_state{}_{}".format(name_list[dev-1], int(state)+1, dep, name_list[dev-1], int(state)+1, dep))
+       #     f.write(f"\n\tif mpc.pid == {dev-1}:")
+       #     f.write("\n\t\tprint('{}_state{}_{}:',await mpc.output({}_state{}_{}_change))".format(name_list[dev-1], int(state)+1, dep, name_list[dev-1], int(state)+1, dep))
         
 
     cond_str = ''
@@ -165,21 +152,44 @@ for dev in range(len(name_list)):
         cond_str = cond_str + truth_val +' + '
     cond_str = cond_str[:-3]
     print(cond_str)
-    #f.write(f"\n\ttruth = {cond_str}")
-    #f.write(f"\n\tif mpc.pid == {dev}:")
-    #f.write(f"\n\t\tprint('Conditions met?:',await mpc.output(temp_check))")
-#if its less than upperThreshold arg=i set "truth" variable to true
 
-#if lowerThreshold arg=i (check existence becuase it might not have both) keep previous variable at true
+    complete_var_list = complete_var_list +[var_list]
+    all_state_changes = all_state_changes + [state_changes]
+    all_comp = all_comp+[comparison_list]
 
 
-#if "truth" changes the value of variable with nameList[i]+stateChange num+combined to be the desired value
-    
+for dev in range(len(name_list)):
+    if dev == 0:
+        f.write(f"\n\tif mpc.pid == 0:")
+    else:
+        f.write(f"\n\telif mpc.pid == {dev}:")
+    counter =1
+    for var in complete_var_list[dev]:
+        f.write(f"\n\t\t{var} = int(sys.argv[{counter}])")
+        counter = counter +1
 
+f.write("\n\n\tasync with mpc:")
+for dev in range(len(name_list)):
+    counter =0
+    for var in complete_var_list[dev]:
+        f.write(f"\n\t\t{var} = mpc.input(secint({var}), {dev})")
+        counter = counter +1
 
+f.write("\n")
+for i in range(len(all_comp)):
+    count = 0
+    for comp in all_comp[i]:
+        f.write(f"{comp}")
+        if(comp[:8]== f"\n\t\ttruth"):
+            #f.write(f"\n\t\tout = await mpc.output(mpc.if_else(truth,{all_state_changes[i][count]} , None), {i})\n")
+            f.write(f"\n\t\tout{i+1}_{count} = await mpc.output(truth{i+1}_{count}, {i})\n")
+            f.write(f"\n\t\tif mpc.pid == {i}:")
+            f.write(f"\n\t\t\tprint(f\"{all_state_changes[i][count]}: {{out{i+1}_{count}}}\")\n\n")
+            count = count+1
 
+print(complete_var_list)
 #shutdown mpc and call main to end file
-f.write("\n\tawait mpc.shutdown()\nmpc.run(main())")
+f.write("\nmpc.run(main())")
 f.close()
  
  
