17c9319a5738aab252e180c2301601566c9f1e3d
AniaK16 <ark2219@columbia.edu>
2024-07-31 15:13:06 -0400

    removing gnome terminal

--- RasPi/run_mpc.sh
+++ RasPi/run_mpc.sh
@@ -5,4 +5,5 @@ args=("$@")
 num=$[${args[0]} - 1 ]
 pid_list=`python3 pid_command.py -n ${num} -p ${args[1]}`
 echo $pid_list
-gnome-terminal -- bash -c "python3 generate_command.py -i device${args[0]}_input.xml -n ${args[0]-1} -p \"${pid_list}\"; bash device1_command.sh 0 ;exec bash;"
+python3 generate_command.py -i device${args[0]}_input.xml -n ${args[0]-1} -p \"${pid_list}\"
+bash device1_command.sh 0
